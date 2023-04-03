e0dccb183038590f2bcf1a21b2ceb8e7a7ac8c3f
Zaky-Git <mzakyf5503@gmail.com>
2023-04-03 08:00:03 +0700

    menambahkan class GlossaryItem1302213067

--- modul7_kelompok_5/Program.cs
+++ modul7_kelompok_5/Program.cs
@@ -2,6 +2,9 @@
 
 DataMahasiswa1302213067 zaky = new DataMahasiswa1302213067();
 TeamMember1302213067 zaky2 = new TeamMember1302213067();
+GlossaryItem1302213067 zaky3 = new GlossaryItem1302213067();
 zaky.ReadJSON();
 Console.WriteLine();
-zaky2.ReadJSON(); 
+zaky2.ReadJSON();
+Console.WriteLine();
+zaky3.ReadJson();
