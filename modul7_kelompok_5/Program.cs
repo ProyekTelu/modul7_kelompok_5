c616743bbfddad965793b7dcce61ffac97d21586
barliantrifqi <barliant.rifqi20@gmail.com>
2023-04-03 07:55:55 +0700

    menambahkan class TeamMembers1302210125

--- modul7_kelompok_5/Program.cs
+++ modul7_kelompok_5/Program.cs
@@ -1,5 +1,10 @@
-﻿using tpmodul7_kelompok_5;
+﻿using modul7_kelompok_5;
+using tpmodul7_kelompok_5;
 
 DataMahasiswa1302210125 bar1 = new DataMahasiswa1302210125();
 
 bar1.ReadJSON();
+
+TeamMember1302210125 bar2 = new TeamMember1302210125();
+
+bar2.ReadJSON();
