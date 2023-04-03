f0603a3ef3e7e627590348888e117a78a2b3294b
barliantrifqi <barliant.rifqi20@gmail.com>
2023-04-03 07:35:17 +0700

    menambahkan class DataMahasiswa1302210125

---
+++ modul7_kelompok_5/DataMahasiswa1302210125.cs
@@ -0,0 +1,32 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tpmodul7_kelompok_5
+{
+    internal class DataMahasiswa1302210125
+    {
+        public void ReadJSON()
+        {
+            string jsonString = File.ReadAllText(@"D:\modul7_kelompok_5\modul7_kelompok_5\jurnal7_1_1302210125.json");
+
+            dynamic jsonObject = JsonConvert.DeserializeObject(jsonString);
+
+            Console.WriteLine("Nama: " + jsonObject.firstName + jsonObject.lastName + "\nGender: " + jsonObject.gender +
+                "\nUmur: " + jsonObject.age + "\nAlamat: " + jsonObject.address.streetAddress + ", " + jsonObject.address.city + 
+                ", " + jsonObject.address.state);
+
+            Console.WriteLine(" ");
+            Console.WriteLine("Daftar mata kuliah yang diambil:");
+            int iter = 1;
+            foreach (var course in jsonObject.courses)
+            {
+                Console.WriteLine($"MK {iter} {course.code}-{course.name}");
+                iter++;
+            }
+        }
+    }
+}
