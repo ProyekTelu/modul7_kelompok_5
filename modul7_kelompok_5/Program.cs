7c5a51797b289ccc3025de6403baa820b3f556ef
Praktikan <fasyaraihan82@gmail.com>
2023-04-03 07:45:41 +0700

    menambahkan class TeamMembers1302213116

--- modul7_kelompok_5/Program.cs
+++ modul7_kelompok_5/Program.cs
@@ -3,3 +3,7 @@
 DataMahasiswa1302213116 objRay1 = new DataMahasiswa1302213116();
 
 objRay1.ReadJSON();
+
+TeamMembers1302213116 objRay2 = new TeamMembers1302213116();
+
+objRay2.ReadJSON();
---
+++ modul7_kelompok_5/TeamMembers1302213116.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace modul7_kelompok_5
+{
+    internal class TeamMembers1302213116
+    {
+        public void ReadJSON()
+        {
+            String jsonString = File.ReadAllText(@"D:\modul7_kelompok_5\modul7_kelompok_5\jurnal7_2_1302213116.json");
+
+            dynamic jsonObj = JsonConvert.DeserializeObject(jsonString);
+
+            Console.WriteLine("\nTEAM MEMBERS \n");
+            foreach (dynamic obj in jsonObj.members)
+            {
+                Console.WriteLine($"Name\t : {obj.firstName} {obj.lastName}\n" +
+                    $"Gender\t : {obj.gender}\n" +
+                    $"Age\t : {obj.age}\n" +
+                    $"nim\t : {obj.nim}\n");
+            }
+        }
+    }
+}
