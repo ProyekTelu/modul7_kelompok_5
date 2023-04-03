4f8f15508aeec71ee4938bf8281b33d6977bcde7
Zaky-Git <mzakyf5503@gmail.com>
2023-04-03 07:17:43 +0700

    menambahkan class DataMahasiswa1302213067

---
+++ modul7_kelompok_5/DataMahasiswa1302213067.cs
@@ -0,0 +1,31 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace modul7_kelompok_5
+{
+    internal class DataMahasiswa1302213067
+    {
+        public void ReadJSON()
+        {
+            string file = File.ReadAllText(@"D:\modul7_kelompok_5\modul7_kelompok_5\jurnal7_1_1302213067.json");
+
+            dynamic jsonFile = JsonConvert.DeserializeObject(file);
+            
+            int i = 1;
+            Console.WriteLine("Nama : " + jsonFile.firstName + " " + jsonFile.lastName + " \n" + "Gender : " + 
+                jsonFile.gender + " \n" + "Umur : " + jsonFile.age + " \n" + "Alamat : " + 
+                jsonFile.address.streetAddress + " " + jsonFile.address.city + " " + jsonFile.address.state);
+
+            foreach (var c in jsonFile.courses)
+            {
+                Console.WriteLine("MK " + i + " " + c.code + "-" + c.name);
+                i++;
+            }
+            
+        }
+    }
+}
--- modul7_kelompok_5/Program.cs
+++ modul7_kelompok_5/Program.cs
@@ -1,1 +1,5 @@
-﻿
+﻿using modul7_kelompok_5;
+
+DataMahasiswa1302213067 zaky = new DataMahasiswa1302213067();
+
+zaky.ReadJSON();
