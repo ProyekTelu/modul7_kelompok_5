4d9357cf677e57e4ecad9a162c98a184d8471a12
Praktikan <fasyaraihan82@gmail.com>
2023-04-03 08:12:26 +0700

    menambahkan class GlossaryItem1302213116

--- modul7_kelompok_5/Program.cs
+++ modul7_kelompok_5/Program.cs
@@ -7,3 +7,7 @@ objRay1.ReadJSON();
 TeamMembers1302213116 objRay2 = new TeamMembers1302213116();
 
 objRay2.ReadJSON();
+
+GlossaryItem1302213116 objRay3 = new GlossaryItem1302213116();
+
+objRay3.ReadJSON();
