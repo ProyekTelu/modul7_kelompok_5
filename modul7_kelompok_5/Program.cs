58fb7547cf1a5601c55a1495b9cdc3fcf166e66d
Zaky-Git <mzakyf5503@gmail.com>
2023-04-03 07:41:16 +0700

    menambahkan class TeamMember1302213067

--- modul7_kelompok_5/Program.cs
+++ modul7_kelompok_5/Program.cs
@@ -1,5 +1,7 @@
 ﻿using modul7_kelompok_5;
 
 DataMahasiswa1302213067 zaky = new DataMahasiswa1302213067();
-
+TeamMember1302213067 zaky2 = new TeamMember1302213067();
 zaky.ReadJSON();
+Console.WriteLine();
+zaky2.ReadJSON(); 
