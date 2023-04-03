7b7d92aa5cb6d23b6349d106e1c19f0ff4fd8f9a
reza <rezadhied@student.telkomuniversity.ac.id>
2023-04-03 07:57:28 +0700

    menambahkan class GlossaryItem1302213016

--- modul7_kelompok_5/Program.cs
+++ modul7_kelompok_5/Program.cs
@@ -5,3 +5,6 @@ MhsReza.ReadJSON();
 
 TeamMember1302213016 MhsReza2 = new TeamMember1302213016();
 MhsReza2.ReadJSON();
+
+GlossaryItem1302213016 MhsReza3 = new GlossaryItem1302213016();
+MhsReza3.ReadJSON();
