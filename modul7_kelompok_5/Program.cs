de810051c4db14f4922e32e24aa81000433cad91
rimuri03 <rizkimr@student.telkomuniversity.ac.id>
2023-04-03 07:51:56 +0700

    menambahkan class TeamMembers1302210097

--- modul7_kelompok_5/Program.cs
+++ modul7_kelompok_5/Program.cs
@@ -3,3 +3,7 @@
 DataMahasiswa1302210097 riz1 = new DataMahasiswa1302210097();
 
 riz1.ReadJSON();
+
+TeamMembers1302210097 riz2   = new TeamMembers1302210097();
+
+riz2.ReadJSON();
---
+++ modul7_kelompok_5/TeamMembers1302210097.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+
+namespace modul7_kelompok_5
+{
+    internal class TeamMembers1302210097
+    {
+        public void ReadJSON()
+        {
+            string jsonriz = File.ReadAllText(@"D:\Modul7_1302210097\modul7_kelompok_5\modul7_kelompok_5\jurnal7_2_1302210097.json");
+
+            dynamic jsonObj = JsonConvert.DeserializeObject(jsonriz);
+
+
+            int i = 1;
+            Console.WriteLine("\nTEAM MEMBERS LIST");
+            foreach (var members in jsonObj.members)
+            {
+                Console.WriteLine( members.firstName + " " + members.lastName + " " + members.gender +" "+ members.age + " " + members.nim);
+                i++;
+            }
+        }
+    }
+}
